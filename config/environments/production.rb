54d36f5d37db1f41e41638454a60e7f3b535fc3c
softechstar302 <softechstar302@gmail.com>
2021-08-02 07:10:38 -0700

    modify production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
